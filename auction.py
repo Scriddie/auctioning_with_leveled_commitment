bac9c659707035d8be5a5cea0de7bb4c3f0074db
Alexander Reisach <alexander.reisach@gmail.com>
2019-12-02 14:44:57 +0100

    some initial comments

---
+++ auction.py
@@ -0,0 +1,24 @@
+# universal starting price per simulation
+
+# r x k auctions
+
+# pure auction
+
+# auction with leveled commitment
+
+# random starting prices s_k element [0, s_{max}]
+
+
+# run
+# set starting price
+# let buyers place bids as multiples of starting price
+# compute market price as avg of all bids
+
+# buyer with highest bid closes to market price is winner
+# that buyer bids second highest bid
+
+# compute seller an buyer profits (for pure and leveled)
+
+# update bids after each round
+
+
